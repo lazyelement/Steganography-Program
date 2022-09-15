7e6d920614650eaf86af832a6c9c1344ebe9ef66
= <=>
2022-09-15 18:47:57 +0800

    edit to toBinary

--- toBinary.py
+++ toBinary.py
@@ -1,23 +1,21 @@
 def decimalToBinary(n):
     return bin(n).replace("0b", "")
 
-with open('imageFile.jpeg','rb') as f:
-    file_content = f.read()
+def fileToBinary(file):
+    
+    with open(file,'rb') as f:
+        file_content = f.read()
 
-f.close()
+    f.close()
 
-output = list(file_content)
-newlist = []
-for i in output:
-    newlist.append(decimalToBinary(int(i)))
+    output = list(file_content)
+    newlist = []
+    for i in output:
+        newlist.append(decimalToBinary(int(i)))
 
-outputBinary = ''
-for i in newlist:
-    outputBinary += i
-
-print(outputBinary)
-
-#Translated the file into binary data (string format)
+    outputBinary = ''
+    for i in newlist:
+        outputBinary += i
 
 
 
